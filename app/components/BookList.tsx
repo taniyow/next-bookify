4c2d04ebcbd188e27ece9557110824c22719cf76
taniyow <markcmtan@gmail.com>
2023-10-09 00:06:02 +0800

    Updates Fetch Function

--- app/components/BookList.tsx
+++ app/components/BookList.tsx
@@ -1,24 +1,25 @@
 'use client';
 import { useQuery } from '@tanstack/react-query';
-import { fetchBooks } from '../api/bookFunctions';
-
-export interface Book {
-  id: number;
-  title: string;
-  author: string;
-  publishedDate: string;
-  genre: string;
-}
+import { fetchBooks } from "../lib/api";
 
 export default function BookList() {
-  const { isLoading, data } = useQuery(['books'], fetchBooks);
-  console.log(data);
+  const { data, error, isLoading } = useQuery(['books'], fetchBooks);
+
+  console.log('Data:', data);
+  console.log('Error:', error);
 
   if (isLoading) return <div>Loading...</div>;
 
   return (
     <div className="flex flex-col gap-2">
-      <h1>Book List</h1>
+      <h2>Books List</h2>
+      <ul>
+        {data?.map((book, index) => (
+          <li key={index}>
+            {book.title} by {book.author}
+          </li>
+        ))}
+      </ul>
     </div>
   )
 }
